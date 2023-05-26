7159e7a9e23ace5274245b3d32b7abdff872c7ba
sayedhany <sayed591999@gmail.com>
2023-05-26 16:12:48 +0300

    add routing settings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Users from "./users/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 function App() {
-  return <h1>Let's start!</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
