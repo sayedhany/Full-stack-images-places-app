0c8d3d91e4d2962cedc2fb40139cb4fec8664f42
sayedhany <sayed591999@gmail.com>
2023-05-30 00:06:47 +0300

    added

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
+
+import Input from '../../shared/components/FormElements/Input';
+import './NewPlace.css';
 
 const NewPlace = () => {
-  return <div>NewPlace</div>;
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[]}
+        errorText="Please enter a valid title."
+      />
+    </form>
+  );
 };
 
 export default NewPlace;
